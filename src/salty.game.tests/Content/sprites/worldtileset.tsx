9f4ec470dacb7c6d4b5fe5b96ea023ce6e4c7e72
Martijn J. Schut <martijnjanschut@gmail.com>
2022-06-30 09:29:13 +0200

    remove salty game tests, not needed

--- src/salty.game.tests/Content/sprites/worldtileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="worldtileset" tilewidth="16" tileheight="16" tilecount="800" columns="20">
- <image source="worldtileset.png" width="320" height="640"/>
-</tileset>
